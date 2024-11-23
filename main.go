7fa06fdeeb3b725cc09b85fffb5a42eb28cf39be
Markus Welscher <markus.welscher01@gmail.com>
2024-11-23 03:18:19 +0100

    replace map Info type with pointer and cut down on unnecessary assignments

--- main.go
+++ main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"bufio"
-	"encoding/csv"
-	"fmt"
 	"log"
 	"os"
 	"runtime/pprof"
 	"sort"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 )
 
@@ -22,12 +19,8 @@ type Info struct {
 	Avg   float64
 }
 
-var weatherData = make(map[string]Info)
-var mapMutex sync.Mutex
-
+var weatherData = make(map[string]*Info)
 var count int
-var countMux sync.Mutex
-
 var now = time.Now()
 
 func main() {
@@ -56,110 +49,60 @@ func main() {
 
 	sort.Strings(weatherStationNames)
 
-	var sortedWeatherData = make(map[string]Info)
-	for _, name := range weatherStationNames {
-		sortedWeatherData[name] = weatherData[name]
+	for _, weatherStation := range weatherStationNames {
+		info := weatherData[weatherStation]
+		log.Println(weatherStation, "Min:", info.Min, "Max:", info.Max, "Avg:", info.Avg)
 	}
 
-	fmt.Println("Elapsed time:", time.Since(start))
-
-	err = writeToCSV(sortedWeatherData)
-	if err != nil {
-		log.Fatalf("failed to write to CSV: %s", err)
-	}
-
-	log.Println("Done")
+	log.Println("Elapsed time:", time.Since(start))
 }
 
 func process() {
 	file, err := os.Open("measurements.txt")
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed opening file: %s", err)
 	}
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		weatherStation, temperature, _ := strings.Cut(line, ";")
-		worker(weatherStation, temperature)
-	}
-}
+		weatherStation, strTemperature, _ := strings.Cut(line, ";")
 
-func worker(weatherStation string, strTemperature string) {
-	temperature, err := strconv.ParseFloat(strTemperature, 64)
-	if err != nil {
-		panic(err)
-	}
-
-	mapMutex.Lock()
-	val, ok := weatherData[weatherStation]
-	if !ok {
-		val = Info{
-			Min:   temperature,
-			Max:   temperature,
-			Sum:   temperature,
-			Count: 1,
-		}
-	} else {
-		info := val
-		if temperature < info.Min {
-			info.Min = temperature
-		}
-		if temperature > info.Max {
-			info.Max = temperature
-		}
-		info.Sum += temperature
-		info.Count++
-		val = info
-	}
-
-	weatherData[weatherStation] = val
-	mapMutex.Unlock()
-
-	countMux.Lock()
-
-	count++
-	if count%1000000 == 0 {
-		log.Println(count)
-		since := time.Since(now).Seconds()
-		ratio := float64(1000000000 / 1000000)
-		estimated := since * ratio / 60
-		log.Println("Estimated time:", estimated, " minutes")
-		now = time.Now()
-	}
-	countMux.Unlock()
-}
-
-func writeToCSV(data map[string]Info) error {
-	file, err := os.Create("weather_data.csv")
-	if err != nil {
-		return fmt.Errorf("failed to create result file: %s", err)
-	}
-	defer file.Close()
-
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	err = writer.Write([]string{"City", "Min", "Max", "Sum", "Count", "Avg"})
-	if err != nil {
-		return fmt.Errorf("failed to write header: %s", err)
-	}
-
-	for city, info := range data {
-		record := []string{
-			city,
-			strconv.FormatFloat(info.Min, 'f', 2, 32),
-			strconv.FormatFloat(info.Max, 'f', 2, 32),
-			strconv.FormatFloat(info.Sum, 'f', 2, 32),
-			strconv.Itoa(info.Count),
-			strconv.FormatFloat(info.Avg, 'f', 2, 32),
-		}
-		err := writer.Write(record)
+		temperature, err := strconv.ParseFloat(strTemperature, 64)
 		if err != nil {
-			return fmt.Errorf("failed to write record: %s", err)
+			log.Fatalf("failed parsing temperature: %s", err)
 		}
+
+		val, ok := weatherData[weatherStation]
+		if !ok {
+			weatherData[weatherStation] = &Info{
+				Min:   temperature,
+				Max:   temperature,
+				Sum:   temperature,
+				Count: 1,
+			}
+		} else {
+
+			if temperature < val.Min {
+				val.Min = temperature
+			}
+			if temperature > val.Max {
+				val.Max = temperature
+			}
+			val.Sum += temperature
+			val.Count++
+		}
+
+		//count++
+		//if count%1000000 == 0 {
+		//	log.Println(count)
+		//	since := time.Since(now).Seconds()
+		//	ratio := float64(1000000000 / 1000000)
+		//	estimated := since * ratio / 60
+		//	log.Println("Estimated time:", estimated, " minutes")
+		//	now = time.Now()
+		//}
 	}
 
-	return nil
 }
