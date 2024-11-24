e8335c057b13763fe9e403153e589f2d5370b629
Markus Welscher <markus.welscher01@gmail.com>
2024-11-24 01:06:52 +0100

    Replace file line scanning with byte chunk scan

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"log"
 	"os"
 	"runtime/pprof"
@@ -63,46 +64,68 @@ func process() {
 		log.Fatalf("failed opening file: %s", err)
 	}
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
+	reader := bufio.NewReader(file)
+	buffer := make([]byte, 4096*4096)
+	var remaining []byte
 
-		weatherStation, strTemperature, _ := strings.Cut(line, ";")
-
-		temperature, err := strconv.ParseFloat(strTemperature, 64)
+	for {
+		byteAmount, err := reader.Read(buffer)
 		if err != nil {
-			log.Fatalf("failed parsing temperature: %s", err)
+			if err.Error() == "EOF" {
+				break
+			}
+
+			log.Fatalf("failed reading file: %s", err)
 		}
 
-		val, ok := weatherData[weatherStation]
-		if !ok {
-			weatherData[weatherStation] = &Info{
-				Min:   temperature,
-				Max:   temperature,
-				Sum:   temperature,
-				Count: 1,
-			}
-		} else {
+		chunk := buffer[:byteAmount]
+		test := bytes.Replace(chunk, []byte("\r"), []byte(""), -1)
+		lines := bytes.Split(test, []byte("\n"))
 
-			if temperature < val.Min {
-				val.Min = temperature
+		for index, line := range lines {
+			if index == len(lines)-1 {
+				remaining = line
+				continue
 			}
-			if temperature > val.Max {
-				val.Max = temperature
+
+			if len(remaining) > 0 {
+				temp := remaining
+				line = append(temp, line...)
+				remaining = nil
 			}
-			val.Sum += temperature
-			val.Count++
+
+			weatherStation, strTemperature, _ := strings.Cut(string(line), ";")
+			temperature, _ := strconv.ParseFloat(strTemperature, 32)
+
+			val, ok := weatherData[weatherStation]
+			if !ok {
+				weatherData[weatherStation] = &Info{
+					Min:   temperature,
+					Max:   temperature,
+					Sum:   temperature,
+					Count: 1,
+				}
+			} else {
+				if temperature < val.Min {
+					val.Min = temperature
+				}
+				if temperature > val.Max {
+					val.Max = temperature
+				}
+				val.Sum += temperature
+				val.Count++
+			}
+
+			//count++
+			//if count%1000000 == 0 {
+			//	log.Println(count)
+			//	since := time.Since(now).Seconds()
+			//	ratio := float64(1000000000 / 1000000)
+			//	estimated := since * ratio / 60
+			//	log.Println("Estimated time:", estimated, " minutes")
+			//	now = time.Now()
+			//}
 		}
-
-		//count++
-		//if count%1000000 == 0 {
-		//	log.Println(count)
-		//	since := time.Since(now).Seconds()
-		//	ratio := float64(1000000000 / 1000000)
-		//	estimated := since * ratio / 60
-		//	log.Println("Estimated time:", estimated, " minutes")
-		//	now = time.Now()
-		//}
 	}
 
 }
